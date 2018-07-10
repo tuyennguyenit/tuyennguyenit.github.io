ee30cc42b0958dffc024fe9f4d2682bf0160701a
Tieu Dao <35517122+tuyennguyenit@users.noreply.github.com>
2018-07-10 09:30:59 +0700

    up len file thu nghiem 1

---
+++ 1.js
@@ -0,0 +1,22 @@
+﻿document.addEventListener('DOMContentLoaded',function(){
+	//khai báo các biến
+	var nut=document.querySelector('.nut'),
+	menutrai=document.querySelector('.menutrai'),
+	den=document.querySelector('.den'),
+	khoito=document.querySelector('.noidungto');
+
+
+	//khi click vào nút xanh
+	nut.onclick=function(){
+		den.classList.add('len');
+
+		//cho các khối to dịch sang phải luôn
+		khoito.classList.add('dichphai');
+	}
+
+	//Khi click vào nền đen
+	den.onclick=function(){
+		den.classList.remove('len');
+		khoito.classList.remove('dichphai');
+	}
+},false)
